103488eb7461e3247b853d8b3012221f1f9f2ed2
joseph-winner <muhanguzijoseph2021@gmail.com>
2024-09-28 23:30:02 +0300

    Adding Navbar

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import News from "./app/Screens/HomeScreen/News/News";
 import RecomendedNews from "./app/Screens/HomeScreen/News/Recomended/RecomendedNews";
 import PopularProducts from "./app/Screens/HomeScreen/PopularProducts/PopularProducts";
 import UpcomingEvents from "./app/Screens/HomeScreen/Events/UpcomingEvents";
+import Navbar from "./app/components/Navbar/Navbar";
 
 
 function App() {
   return (
     <div className="app">
+      <Navbar />
       <Banner />
       <Featured />
       <Categories />
---
+++ src/app/components/Navbar/Navbar.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import Logo from "../../../utils/img/logo-nobg.png";
+import "./Navbar.css";
+import { FaBars, FaPlus } from 'react-icons/fa';
+
+export default function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 500) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav className={isScrolled ? 'scrolled' : ''}>
+      <div className="nav-items">
+        <div className="nav-logo">
+          <img src={Logo} alt="Africans in Doha Logo" />
+        </div>
+        <div className={`nav-items-lists ${isMenuOpen ? 'open' : ''}`}>
+          <ul>
+            <li><a href="/">News</a></li>
+            <li><a href="/">Buy</a></li>
+            <li><a href="/">Sell</a></li>
+            <li><a href="/">Events</a></li>
+            <li><a href="/">Community</a></li>
+          </ul>
+        </div>
+        <div className="nav-btns">
+          <div className="add-post">
+            <p className='responsive-bg'>Post Ad</p>
+            <p className="responsive-sm" title='Post Ad'>
+              <FaPlus />
+            </p>
+          </div>
+          <div className="login-btn">
+            <p>Login</p>
+          </div>
+        </div>
+        <div className="humberger" onClick={toggleMenu}>
+          <FaBars />
+        </div>
+      </div>
+    </nav>
+  );
+}
